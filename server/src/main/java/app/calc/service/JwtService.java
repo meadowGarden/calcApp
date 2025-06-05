477b706d800e9adaf7c84b5ebe5b8370a9704008
dariusLenovoPC <jomantasdarius@yahoo.com>
2025-06-05 10:15:43 +0300

    finalized version

--- server/src/main/java/app/calc/service/JwtService.java
+++ server/src/main/java/app/calc/service/JwtService.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = a1;
+    private static final String SECRET_KEY = ;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
